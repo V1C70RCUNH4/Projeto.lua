55cf5356f7a08eb09e3b283ec0e45efa0187f642
Victor Pereira da Cunha <victor.pereira.cunha@outlook.com>
2024-08-07 00:12:44 -0300

    Add files via upload

---
+++ Aula 06-Algoritmos e Funções.lua
@@ -0,0 +1,35 @@
+ -- ALGORITMO: tarefa em ordem lógica e sequencial com a intenção de cumprir
+ -- um objetivo pré-definido.
+
+ -- Um monte de tarefas, uma depois da outra, afim de terminar alguma coisa.
+ -- ou com o objetivo de concluir uma tarefa específica.
+
+
+ --[[
+
+ ALGORITMO: JOGAR O PAPEL NO LIXO
+ 1) Pegar o papel
+ 2) Amassar o papel
+ 3) Encontrar a lixeira
+ 4) Jogar o papel na lixeira
+
+ ALGORITMO: Escovar os Dentes
+
+ 1) Ir ao banheiro
+ 2) Abrir o armário
+ 3) Pegar a Escova
+ 4) Pegar a bisnaga com pasta de Dente
+ 5) Abrir a bisnaga
+ 6) Colocar a pasta na Escovar
+ 7) Fechar a bisnaga
+ 8) Molhar a Escovar
+ 9) Mover a escova para a boca
+ 10) Escovar os Dentes
+ 11) Cuspir na pia
+ 12) Guardar a Escova
+ 13) Abrir a torneira
+ 14) Pegar água
+ 15) Enxaguar a boca
+ 16) Cuspir na pia
+
+ ]]--
