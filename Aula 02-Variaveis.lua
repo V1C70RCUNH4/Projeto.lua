3855cda0f7d29112a6de2f1e43b72a3fa3cb7193
Victor Pereira da Cunha <victor.pereira.cunha@outlook.com>
2024-08-07 00:52:16 -0300

    Add files via upload

---
+++ Aula 02-Variaveis.lua
@@ -0,0 +1,6 @@
+olaMundo = "Olá Mundo"
+olaMundo = "olá Mundo Vamos Interagir"
+
+-- 1) Variaveis não podem ter acentos ou caracteres especiais.
+-- Letras, numeros, Undescore_
+-- 2) Variaveis não podem ter espaços
