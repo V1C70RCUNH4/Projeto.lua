117fc18b59ad33832f555791fcab17299725ad83
Victor Pereira da Cunha <victor.pereira.cunha@outlook.com>
2024-08-07 09:59:23 -0300

    Add files via upload

---
+++ Aula 02-Variaveis-02.lua
@@ -0,0 +1,16 @@
+
+parte1 = "parte 1"
+parte2 = "parte 2"
+print(parte1 .." Victor "..parte2)
+
+
+titulo = "Você me deve: "
+valor = 450
+print(titulo .."R$" .. valor)
+
+
+--[[ Somar numeros: 4 + 5
+juntar(Concatenar 2 textos): "text".."text"
+Concatenar numeros com textos: 4 .. "text"
+Concatenar textos com numeros: "text" .. 4
+]]--
