e05703d8dab7652a5f8c4fa31824eae41c09352b
Victor Pereira da Cunha <victor.pereira.cunha@outlook.com>
2024-08-08 06:10:15 -0300

    Add files via upload

---
+++ Aula 03-Valor boolean.lua
@@ -0,0 +1,35 @@
+-- boolean só pode assumir dois possiveis valores:
+-- VERDADEIRO OU FALSO
+-- treu
+-- false
+
+ estaAtivo = false
+
+ print(estaAtivo)
+
+ --== é o sinal de comparação de igualdade.
+ --== compara se alguma coisa é igual a outra.
+ print(estaAtivo == false)
+
+ estaInativo = true
+ print("Valor de estaInativo: ".. tostring(estaInativo))
+ print("Está inativo? " .. tostring(estaInativo == true))
+
+
+ --[[
+ >
+ <
+ >=
+ <=
+ ==
+ RETORNAMM UM VALOR BOOLEAN:
+ SEMPRE TRUE OU FALSE
+ ]]--
+
+ --[[
+ tostring vem do inglês TO STRING, ou seja, PARA STRING
+ tostring(true) ==>"true"
+
+ Converte o número 123 ara o texto "123":
+ tostring(123) ==> "123"
+ ]]--
