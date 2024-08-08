b7a317e843c5a72edf42c431eae99a0cafd18dcf
Victor Pereira da Cunha <victor.pereira.cunha@outlook.com>
2024-08-08 06:29:01 -0300

    Add files via upload

---
+++ Aula 04-nil.lua
@@ -0,0 +1,13 @@
+-- null
+-- nil
+
+-- ... sgnifica "Nulo"!
+
+umaVariavel = nil
+print(umaVariavel)
+
+umaVariavel = "um valor qualquer"
+print(umaVariavel)
+
+umaVariavel = nil
+print(tostring(umaVariavel == "um valor qualquer"))
